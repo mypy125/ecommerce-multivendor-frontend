417ff699ae13d8348acf49714a4505ca5e134ec8
mohito <mypywelcome@gmail.com>
2025-02-23 14:22:37 +0400

    added_product-sections

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import Navbar from './customer/components/navbar/Navbar.tsx';
 import { ThemeProvider } from '@mui/material';
 import customerTheme from './theme/customerTheme.ts';
 import Home from './customer/pages/home/Home.tsx';
+import Product from './customer/pages/product/Product.tsx';
 
 function App() {
   return (
     <ThemeProvider theme={customerTheme}>
       <div>
         <Navbar/>
-        <Home/>
+        {/* <Home/> */}
+        <Product/>
       </div>
       
     </ThemeProvider>
---
+++ src/customer/pages/product/Product.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import FilterSection from "./FilterSection.tsx";
+import ProductCard from "./ProductCard.tsx";
+import { Divider, FormControl, IconButton, InputLabel, MenuItem, Select, 
+    useMediaQuery, useTheme 
+} from "@mui/material";
+import { FilterAlt } from "@mui/icons-material";
+
+const Product = () => {
+    const them = useTheme();
+    const isLarge = useMediaQuery(them.breakpoints.up("lg"));
+    const [sort,setSort]=useState();
+
+    const handleSortChange = (event:any) => {
+        setSort(event.taget.value)
+    }
+
+    return(
+        <div className="-z-10- mt-10">
+            <div>
+                <h1 className="text-3xl text-center font-bold text-gray-700 pb-5 px-9 uppercase spce-x-2">
+                    women sarees
+                </h1>
+                <div className="lg:flex">
+                    <section className="filter_section hidden lg:block w-[20%]">
+                        <FilterSection/>
+                    </section>
+                    <div className="w-full lg:w-[80%] space-y-5">
+                        <div className="flex justify-between items-center px-9 h-[40px]">
+                            <div className="relative w-[50%]">
+                                {
+                                    !isLarge && (<IconButton>
+                                        <FilterAlt/>
+                                    </IconButton>)
+                                }
+
+                            </div>
+                            <FormControl size="small" sx={{width:"200px"}}>
+                              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                              <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={sort}
+                                label="Age"
+                                onChange={handleSortChange}
+                              >
+                                <MenuItem value={"price_low"}>Price : Low - High</MenuItem>
+                                <MenuItem value={"price_high"}>Price : High - Low</MenuItem>
+                              </Select>
+                            </FormControl>
+                        </div>
+
+                        <Divider/>
+
+                        <section className="products_section">
+                            <ProductCard/>
+                        </section>
+                    </div>
+                 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Product;
