f76cc124814613e9549bf41d344c9336be1b31fd
mohito <mypywelcome@gmail.com>
2025-03-09 16:19:36 +0400

    added_orderitem

--- src/customer/pages/account/OrderItem.tsx
+++ src/customer/pages/account/OrderItem.tsx
@@ -1,8 +1,46 @@
 import React from "react";
+import {Avatar} from '@mui/material';
+import { ElectricBolt } from "@mui/icons-material";
+import { teal } from "@mui/material/colors";
 
 const OrderItem = () => {
     return(
-       <div>OrderItem</div>
+       <div className="text-sm bg-white p-5 space-y-4 border rounded-md cursor-pointer">
+
+        <div className="flex items-center gap-5">
+
+            <div>
+                <Avatar sizes="small" sx={{bgcolor:teal[500]}}>
+                    <ElectricBolt/>
+                </Avatar>
+            </div>
+            <div>
+                <h1 className="font-bold text-primary-color">
+                    PENDING
+                </h1>
+                <p>Arriving By Mont, 15 march</p>
+            </div>
+        </div>
+        <div className="p-5 bg-teal-50 flex gap-3">
+            <div>
+                <img className="w-[70px]" 
+                    src="https://img.freepik.com/free-vector/blond-man-with-eyeglasses-icon-isolated_24911-100831.jpg?semt=ais_hybrid"
+                 alt="" 
+                />
+            </div>
+            <div className="w-full space-y-2">
+                <h1 className="font-bold">Armani Clothing</h1>
+                <p>Cellecor A9 Pro Stark Bluetooth Calling Smart Watch with 1.96"
+                     LCD Display, SpO2 & Heart Rate Monitor, IP68 Water Resistant, 5 Days.
+                </p>
+                <p>
+                    <strong>Size: </strong>
+                    FREE
+                </p>
+            </div>
+        </div>
+        
+       </div>
     )
 }
 
--- src/customer/pages/account/Orders.tsx
+++ src/customer/pages/account/Orders.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import OrderItem from "./OrderItem.tsx";
 
 const Orders = () => {
     return(
-        <div>Orders</div>
+        <div className="text-sm min-h-screen">
+            <div className="pb-5">
+                <h1 className="font-semibold">All Orders</h1>
+                <p>form anythime</p>
+            </div>
+            <div className="space-y-2">
+                {
+                    [...Array(6)].map((item)=> <OrderItem/>)
+                }
+            </div>
+
+        </div>
     )
 }
 
