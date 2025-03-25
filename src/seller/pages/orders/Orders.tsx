e043633dd232c3068a12ffad09f3266eba1e77ab
mohito <mypywelcome@gmail.com>
2025-03-25 14:57:57 +0400

    added__product-table_and_order-table_in_seller-dashboard-tool

--- src/seller/pages/orders/Orders.tsx
+++ src/seller/pages/orders/Orders.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import OrderTable from "./OrderTable.tsx";
 
 const Orders = () => {
     return(
-        <div>Orders</div>
+        <div>
+            <h1 className="font-bold mb-5 text-xl">All Orders</h1>
+            <OrderTable/>
+        </div>
     )
 }
 
