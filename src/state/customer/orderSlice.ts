07c763c967dd5a036e84614c1c78af36565eb662
mohito <mypywelcome@gmail.com>
2025-05-08 15:58:22 +0400

    added_methods_for_fetchOrderItemById_paymentSuccess_cancelOrder

--- src/state/customer/orderSlice.ts
+++ src/state/customer/orderSlice.ts
@@ -1,7 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { Order, OrderState } from "../../types/orderTypes.ts";
+import { Order, OrderItem, OrderState } from "../../types/orderTypes.ts";
 import { api } from "../../config/Api.ts";
 import { Address } from "../../types/userTypes.ts";
+import axios from "axios";
 
 const API_URL="/api/orders";
 
@@ -74,6 +75,79 @@ export const createOrder=createAsyncThunk<any,{address:Address,jwt:string,paymen
         } catch (error:any) {
             console.log("error ",error.response)
             return rejectWithValue("failed to create order")
+        }
+    }
+)
+
+export const fetchOrderItemById=createAsyncThunk<OrderItem,{orderItemId:number,jwt:string}>(
+    "orders/fetchOrderItemById",
+    async ({orderItemId,jwt},{rejectWithValue})=>{
+        try {
+            const response = await api.get(`${API_URL}/item/${orderItemId}`,{
+                headers:{
+                    Authorization: `Bearer ${jwt}`,
+                },
+            });
+            console.log("order Item fetched ",response.data);
+            return response.data;
+        } catch (error:any) {
+            console.log("error ",error.response)
+            rejectWithValue(
+                "failed to fetch order Item By Id"
+            )
+        }
+    }
+)
+
+export const paymentSuccess=createAsyncThunk<any,
+    {paymentId:string,jwt:string,paymentLinkId:string},{rejectValue:string}>(
+    "orders/paymentSuccess",
+    async ({paymentId,jwt,paymentLinkId},{rejectWithValue})=>{
+        try {
+            const response=await api.get(`api/payment/${paymentId}`,{
+                headers:{
+                    Authorization: `Bearer ${jwt}`,
+                },
+                params:{
+                    paymentLinkId
+                }
+            });
+            console.log("apyment success ",response.data);
+            return response.data;
+        } catch (error:any) {
+            console.log("error ",error.response)
+            if(error.response){
+                return rejectWithValue(
+                    error.response.data.message
+                )
+            }
+            return rejectWithValue(
+               "failed to process payment"
+            )
+            
+        }
+    }
+)
+
+export const cancelOrder=createAsyncThunk<Order,any>(
+    "orders/cancelOrder",
+    async (orderId,{rejectWithValue})=>{
+        try {
+            const response=await api.put(`${API_URL}/${orderId}/cancel`,{},{
+                headers:{
+                    Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+                },
+            });
+            console.log("cancel order ",response.data);
+            return response.data;
+        } catch (error:any) {
+            console.log("error ",error.response);
+            if(axios.isAxiosError(error)&&error.response){
+                return rejectWithValue(
+                    error.response.data
+                )
+            }
+            return rejectWithValue("error occurred while canceling the order")
         }
     }
 )
